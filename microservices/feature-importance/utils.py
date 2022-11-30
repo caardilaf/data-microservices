ca38ab975046c423049534bc1b1d42ba6e225bf9
César Ardila <80080665+cardilaf@users.noreply.github.com>
2022-11-29 23:17:51 -0500

    Finish --sort_dictionary_by_value-- function.

--- microservices/feature-importance/utils.py
+++ microservices/feature-importance/utils.py
@@ -1,15 +1,29 @@
 """Utility functions"""
-from collections.abc import Iterable
 
 def sort_dictionary_by_value(dict_input: dict) -> dict:
+    """
+    It sorts a dictionary by value.
+    
+    Args:
+      dict_input (dict): dict
+    
+    Returns:
+      A dictionary with the same keys as the input dictionary, but with the values sorted in descending
+    order.
+    """
 
     # Validate input
     if not isinstance(dict_input, dict):
         raise TypeError("--dict_input-- must be dict type.")
     
-    iterables = [isinstance(val, Iterable) for val in dict_input.values()]
-    if not all(iterables):
-        raise TypeError("--dict_input-- values can't be iterables.")
+    valid_types = [isinstance(val, (str, int, float)) for val in dict_input.values()]
+    if not all(valid_types):
+        raise TypeError("--dict_input-- values must be strings, integers or floats.") 
+
+    first_type = type(list(dict_input.values())[0])
+    same_types = [isinstance(val, first_type) for val in dict_input.values()]
+    if not all(same_types):
+        raise TypeError("--dict_input-- values must be of the same type.")
 
     # Sorting the input
     sorted_dict = {
